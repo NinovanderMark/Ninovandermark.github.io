1de2731cfa715433127c86d68285e15423b694e0
Nino van der Mark <ninovdmark@gmail.com>
2024-07-29 11:49:13 +0200

    Reworked personal website to be a single-pager

--- docs/scripts/footer.js
+++ docs/scripts/footer.js
@@ -1,11 +1,11 @@
 (function() {
     let messages = [
-        "Website is powered by the <span class='glow'>Holy Spirit</span> and hosted on <a href='https://github.com/NinovanderMark/Ninovandermark.github.io'>GitHub Pages</a>!",
-        "Website is powered by the grace of God and hosted on <a href='https://github.com/NinovanderMark/Ninovandermark.github.io'>GitHub Pages</a>!",
-        "Website is powered by the love of Jesus and hosted on <a href='https://github.com/NinovanderMark/Ninovandermark.github.io'>GitHub Pages</a>!",
-        "Website is powered by HTML5 and hosted on <a href='https://github.com/NinovanderMark/Ninovandermark.github.io'>GitHub Pages</a>!",
-        "Website is powered by a history of mucking about with computers and hosted on <a href='https://github.com/NinovanderMark/Ninovandermark.github.io'>GitHub Pages</a>!",
-        "Website is powered by random number generators and hosted on <a href='https://github.com/NinovanderMark/Ninovandermark.github.io'>GitHub Pages</a>!"
+        "Powered by the <span class='glow'>Holy Spirit</span> and hosted on <a href='https://github.com/NinovanderMark/Ninovandermark.github.io'>GitHub Pages</a>!",
+        "Powered by the grace of God and hosted on <a href='https://github.com/NinovanderMark/Ninovandermark.github.io'>GitHub Pages</a>!",
+        "Powered by the love of Jesus and hosted on <a href='https://github.com/NinovanderMark/Ninovandermark.github.io'>GitHub Pages</a>!",
+        "Powered by HTML5 and hosted on <a href='https://github.com/NinovanderMark/Ninovandermark.github.io'>GitHub Pages</a>!",
+        "Powered by a history of mucking about with computers and hosted on <a href='https://github.com/NinovanderMark/Ninovandermark.github.io'>GitHub Pages</a>!",
+        "Powered by random number generators and hosted on <a href='https://github.com/NinovanderMark/Ninovandermark.github.io'>GitHub Pages</a>!"
     ];
 
     if ( randomNumber(4) === 3)
@@ -16,5 +16,5 @@
     if ( p.length < 1)
         return;
 
-    p[0].innerHTML = messages[index];
+    p[0].innerHTML = `Copyright ©2024 Nino van der Mark - ${messages[index]}`;
 })()
