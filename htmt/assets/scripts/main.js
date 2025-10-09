9df12bb6cccbccedeefdfc9af6df03d39a099774
Nino van der Mark <ninovdmark@gmail.com>
2025-10-09 13:06:29 +0200

    Added missing 'programming' folder Merged footer and main page templates

--- htmt/assets/scripts/main.js
+++ htmt/assets/scripts/main.js
@@ -23,3 +23,24 @@ function addScript(filepath) {
 function randomNumber(max) {
     return Math.floor(Math.random() * max);
 }
+
+(function() {
+    let messages = [
+        "Powered by the <span class='glow'>Holy Spirit</span> and hosted on <a href='https://github.com/NinovanderMark/Ninovandermark.github.io'>GitHub Pages</a>!",
+        "Powered by the grace of God and hosted on <a href='https://github.com/NinovanderMark/Ninovandermark.github.io'>GitHub Pages</a>!",
+        "Powered by the love of Jesus and hosted on <a href='https://github.com/NinovanderMark/Ninovandermark.github.io'>GitHub Pages</a>!",
+        "Powered by HTML5 and hosted on <a href='https://github.com/NinovanderMark/Ninovandermark.github.io'>GitHub Pages</a>!",
+        "Powered by a history of mucking about with computers and hosted on <a href='https://github.com/NinovanderMark/Ninovandermark.github.io'>GitHub Pages</a>!",
+        "Powered by random number generators and hosted on <a href='https://github.com/NinovanderMark/Ninovandermark.github.io'>GitHub Pages</a>!"
+    ];
+
+    if ( randomNumber(4) === 3)
+        return;
+
+    let index = randomNumber(messages.length);
+    let p = document.querySelectorAll('footer p');
+    if ( p.length < 1)
+        return;
+
+    p[0].innerHTML = `Copyright ©2025 Nino van der Mark - ${messages[index]}`;
+})()
